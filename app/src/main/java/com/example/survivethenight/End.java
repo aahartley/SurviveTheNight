b8c0b9063cd97f6cf63ac4178d4283340d1906bb
Austin Hartley <50050814+aahartley@users.noreply.github.com>
2020-08-28 14:25:27 -0400

    Added end, and countdown, fixed up code

---
+++ app/src/main/java/com/example/survivethenight/End.java
@@ -0,0 +1,16 @@
+package com.example.survivethenight;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class End extends AppCompatActivity {
+    TextView tV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+      //  tV.setText(R.string.youLose);
+    }
+}
