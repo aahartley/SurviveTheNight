d8180f2bd85ab903a12980eaf45d13074fb136e7
Austin Hartley <50050814+aahartley@users.noreply.github.com>
2020-08-29 12:59:54 -0400

    update

--- app/src/main/java/com/example/survivethenight/Game.java
+++ app/src/main/java/com/example/survivethenight/Game.java
@@ -1,6 +1,11 @@
 package com.example.survivethenight;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -10,10 +15,18 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Game extends AppCompatActivity {
+public class Game extends AppCompatActivity implements SensorEventListener {
     public int bullets =2;
     private boolean shot = false;
     public int counter =0;
+    private SensorManager sM;
+    private Sensor aS;
+    private boolean aSA;
+    private boolean notFirst = false;
+    private float lastX;
+    private float lastY;
+    private float lastZ;
+    public boolean reload = false;
     MediaPlayer mP;
     MediaPlayer mP2;
 
@@ -21,6 +34,16 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        sM = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        assert sM != null;
+        if(sM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!=null){
+            aS = sM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            aSA = true;
+        }
+        else{
+            aSA=false;
+        }
 
         mP = MediaPlayer.create(this,R.raw.walking);
         mP.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -37,14 +60,21 @@ public class Game extends AppCompatActivity {
 
 
         final TextView tV = findViewById(R.id.time2);
+        final TextView tv2 = findViewById(R.id.time3);
 
         new CountDownTimer(20000,1000){
             @Override
             public void onTick(long millisUntilFinished) {
                 tV.setText((String.valueOf(counter)));
+                mP2 = MediaPlayer.create(Game.this, R.raw.gun);
+
                 counter++;
+
+                if(reload){
+                    mP2.start();
+                    reload = false;
+                }
                 if(counter ==5){
-                    mP2 = MediaPlayer.create(Game.this, R.raw.gun);
                     mP2.start();
                     shot = false;
                 }
@@ -58,7 +88,6 @@ public class Game extends AppCompatActivity {
                         mP2.start();
                     }
                          if(counter ==12 && !shot) {
-                            tV.setText(R.string.youLose);
                             cancel();
                 }
 
@@ -78,19 +107,26 @@ public class Game extends AppCompatActivity {
 
         final Button bt = findViewById(R.id.button);
 
+
+
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                shot = true;
-                bullets --;
                 mP2 = MediaPlayer.create(Game.this,R.raw.gun);
-                mP2.start();
+                if(bullets >0) {
+                    mP2.start();
+                    bullets --;
+                    tv2.setText(String.valueOf(bullets));
+                    shot=true;
+
+                }
+                else
+                    shot = false;
 
             }
         });
-        if(bullets ==0){
-            bt.setEnabled(false);
-        }
+
+
 
 
 
@@ -101,4 +137,51 @@ public class Game extends AppCompatActivity {
         finish();
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float currentX = event.values[0];
+        float currentY = event.values[1];
+        float currentZ = event.values[2];
+
+        if(notFirst){
+            float xDiff = Math.abs(lastX - currentX);
+            float yDiff = Math.abs(lastY - currentY);
+            float zDiff = Math.abs(lastZ - currentZ);
+            float shakeThreshold = 5f;
+            if((xDiff > shakeThreshold && yDiff > shakeThreshold) ||
+                    (xDiff > shakeThreshold && zDiff > shakeThreshold) ||
+                    (yDiff > shakeThreshold && zDiff > shakeThreshold)){
+                bullets=6;
+                reload=true;
+
+                onPause();
+            }
+
+        }
+        lastX= currentX;
+        lastY = currentY;
+        lastZ = currentZ;
+        notFirst = true;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(aSA){
+            sM.registerListener(this,aS,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(aSA){
+            sM.unregisterListener(this);
+        }
+    }
 }
